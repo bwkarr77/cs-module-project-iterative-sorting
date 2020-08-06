138a8bb038d22c39be5c889bbd1275619ab90a06
Brett Karr <b.karr@polyclip.us>
2020-08-05 21:41:56 -0500

    all files completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(len(arr)):
+        # print('linear search: ', arr[i], target)
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
-
-
+    # print('binary search: ', arr)
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        if target > arr[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
     return -1  # not found
